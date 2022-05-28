dbe6746090860672ce0e182af0490b18c6f2ba2c
farah <farah.fournat@gmail.com>
2022-05-28 22:30:08 +0200

    CartButton completed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,14 @@
-import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <span></span>
-      <span></span>
-      <span></span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
